78f1d470a4adac48892ac77bae98a4fc26b2aec9
Juan Ibiapina <juanibiapina@gmail.com>
2012-08-16 22:15:23 -0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stretchsearch::Application.config.secret_token = '5c285fe3c0a14ae1a40f02ad390f571964fbcc0ba3c66ed024aea9f0327616e0cc0519698b490f67e970c58725c3d9f12cbd7330d3de0797b228c729b6235d79'
