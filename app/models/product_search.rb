a95a558db728b05aa20cef4f8fbdb379800b5daa
Juan Ibiapina <juanibiapina@gmail.com>
2012-08-18 20:42:07 -0300

    Idea for matchers.

--- app/models/product_search.rb
+++ app/models/product_search.rb
@@ -1,5 +1,5 @@
 class ProductSearch
   include StretchSearch
 
-  search :name
+  search :name, :contains
 end
--- app/models/stretch_search.rb
+++ app/models/stretch_search.rb
@@ -1,9 +1,18 @@
+class FieldRule
+  attr :field, :method
+
+  def initialize(field, method)
+    @field = field
+    @method = method
+  end
+end
+
 module StretchSearch
   extend ActiveSupport::Concern
 
   included do
-    class_attribute :_fields, :_target_class
-    self._fields = []
+    class_attribute :_rules, :_target_class
+    self._rules = []
     self._target_class = name[0..-7].constantize
   end
 
@@ -17,21 +26,27 @@ module StretchSearch
 
   def result
     result = _target_class.scoped
-    _fields.each do |f|
-      value = send(f)
+    _rules.each do |r|
+      field = r.field
+      method = r.method
+      value = send(field)
       if value.present?
-        result = result.where(f => value)
+        if method == :eq
+          result = result.where(field => value)
+        elsif method == :contains
+          result = result.where("#{field} LIKE '%#{value}%'")
+        end
       end
     end
     result
   end
 
   module ClassMethods
-    def search(field)
+    def search(field, method)
       class_eval do
         attr_accessor field
       end
-      _fields << field
+      _rules << FieldRule.new(field, method)
     end
   end
 end
