932e191eb6aa7508087699a9a8478656bf6db297
Juan Ibiapina <juanibiapina@gmail.com>
2012-08-18 17:45:54 -0300

    Move StretchSearch module to it's own file.

---
+++ app/models/stretch_search.rb
@@ -0,0 +1,36 @@
+module StretchSearch
+  extend ActiveSupport::Concern
+
+  included do
+    class_attribute :_fields
+    self._fields = []
+  end
+
+  def initialize(params)
+    if params.present?
+      params.each do |name, value|
+        send "#{name}=", value
+      end
+    end
+  end
+
+  def result
+    result = Product.scoped
+    _fields.each do |f|
+      value = send(f)
+      if value.present?
+        result = result.where(f => value)
+      end
+    end
+    result
+  end
+
+  module ClassMethods
+    def search(field)
+      class_eval do
+        attr_accessor field
+      end
+      _fields << field
+    end
+  end
+end
